ec2e91d0e8a2e60b6168c2ca2691a9e344af7d0c
Suyun514 <suyun514@qq.com>
2020-04-14 16:12:57 +0800

    Fix a bug

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const Koa = require('koa');
 const Router = require('koa-router');
+const cors = require('@koa/cors');
 const static = require('koa-static');
 const bodyParser = require('koa-bodyparser');
 
@@ -12,6 +13,7 @@ const database = require('./database');
 let app = new Koa();
 let router = new Router();
 
+app.use(cors());
 app.use(static('.'));
 app.use(bodyParser());
 app.use(router.middleware());
@@ -27,13 +29,24 @@ router.get('/api/queryAllUsers', async (ctx) => {
 });
 
 router.get('/api/query', async (ctx) => {
-    ctx.body = (await database.getPerson(ctx.request.query.id));
+    let id = ctx.request.query.id;
+    
+    if (id >= 1 && id <= config.persons) {
+        ctx.body = (await database.getPerson(ctx.request.query.id));
+    } else {
+        ctx.body = "编号超出范围！";
+    }
 });
 
 router.post('/api/poll', async (ctx) => {
     let { id, uid } = ctx.request.body;
 
     let user = await database.getUser(uid);
+
+    if (!(uid >= 1 && uid <= config.users)) {
+        ctx.body = { error: true, msg: "学号超出范围！" };
+        return;
+    }
 
     if (user.count.length >= config.maxPoll) {
         ctx.body = { error: true, msg: `您已经投了 ${user.count.length} 票了，不能再投了！` };
@@ -46,6 +59,12 @@ router.post('/api/poll', async (ctx) => {
     }
 
     let person = await database.getPerson(id);
+    
+    if (!(id >= 1 && id <= config.persons)) {
+        ctx.body = { error: true, msg: "编号超出范围！" };
+        return;
+    }
+    
     await database.updateParson(id, parseInt(person.count) + 1);
     await database.updateUser(uid, id);
 
