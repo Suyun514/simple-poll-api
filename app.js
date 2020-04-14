b1b0fb45c36ab37c2612a2eaabd9a4af1c88072c
Suyun514 <suyun514@qq.com>
2020-04-14 11:27:15 +0800

    Finished!

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const Koa = require('koa');
 const Router = require('koa-router');
+const static = require('koa-static');
 const bodyParser = require('koa-bodyparser');
 
 const config = require('./config.json');
@@ -11,17 +12,44 @@ const database = require('./database');
 let app = new Koa();
 let router = new Router();
 
+app.use(static('.'));
 app.use(bodyParser());
 app.use(router.middleware());
 
-database.init();
-
-fs.readdirSync(path.join(__dirname, 'modules'))
-    .filter((file) => file.endsWith('.js'))
-    .forEach((file) => require(path.join(__dirname, 'modules', file)));
+database.init(config.persons, config.users);
 
 router.get('/api/queryAll', async (ctx) => {
-    ctx.body = database.getPersons();
+    ctx.body = await database.getPersons();
+});
+
+router.get('/api/queryAllUsers', async (ctx) => {
+    ctx.body = await database.getUsers();
+});
+
+router.get('/api/query', async (ctx) => {
+    ctx.body = (await database.getPerson(ctx.request.query.id));
+});
+
+router.post('/api/poll', async (ctx) => {
+    let { id, uid } = ctx.request.body;
+
+    let user = await database.getUser(uid);
+
+    if (user.count.length >= config.maxPoll) {
+        ctx.body = { error: true, msg: `您已经投了 ${user.count.length} 票了，不能再投了！` };
+        return;
+    }
+
+    if (user.count.indexOf(parseInt(id)) != -1) {
+        ctx.body = { error: true, msg: "您已经为这个人投过票了！" };
+        return;
+    }
+
+    let person = await database.getPerson(id);
+    await database.updateParson(id, parseInt(person.count) + 1);
+    await database.updateUser(uid, id);
+
+    ctx.body = { error: false, msg: "投票成功！" };
 });
 
 app.listen(process.argv[2] ? process.argv[2] : 80);
--- database.js
+++ database.js
@@ -12,15 +12,57 @@ const client = new Client({
 client.connect();
 
 module.exports = {
+    async getPerson(id) {
+        return (await client.query(`SELECT * FROM persons WHERE id = ${id}`)).rows[0];
+    },
+
     async getPersons() {
         return (await client.query('SELECT * FROM persons')).rows;
     },
 
+    async updateParson(id, count) {
+        return (await client.query(`UPDATE persons SET count = ${count} WHERE id = ${id}`));
+    },
+
+    async getUser(uid) {
+        return (await client.query(`SELECT * FROM users WHERE uid = ${uid}`)).rows[0];
+    },
+
+    async getUsers() {
+        return (await client.query('SELECT * FROM users')).rows;
+    },
+
+    async updateUser(uid, id) {
+        return (await client.query(`UPDATE users SET count = ARRAY_APPEND(count, ${id}) WHERE uid = ${uid}`));
+    },
+
     async init() {
         try {
-            await this.getPersons();
+            let persons = await this.getPersons();
+            if (persons.length != config.persons) {
+                throw new Error;
+            }
         } catch (e) {
-            await client.query('CREATE TABLE persons (id bigint, count bigint)');
+            try { await client.query('DROP TABLE persons'); } catch(e) {}
+            await client.query('CREATE TABLE persons (id int, count int)');
+
+            for (let i = 1; i <= config.persons; ++i) {
+                await client.query(`INSERT INTO persons (id, count) VALUES (${i}, 0)`);
+            }
+        }
+
+        try {
+            let users = await this.getUsers();
+            if (users.length != config.users) {
+                throw new Error;
+            }
+        } catch (e) {
+            try { await client.query('DROP TABLE users'); } catch(e) {}
+            await client.query('CREATE TABLE users (uid int, count int[])');
+
+            for (let i = 1; i <= config.users; ++i) {
+                await client.query(`INSERT INTO users (uid, count) VALUES (${i}, '{}')`);
+            }
         }
     }
 };
